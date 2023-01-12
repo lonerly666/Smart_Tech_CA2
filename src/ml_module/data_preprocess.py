adf86eb1b8a5d0cde1acbfbc6bbb9a1afc2a68b6
Jeremy Neo <doremineo@gmail.com>
2023-01-12 23:45:34 +0000

    added image preprocess

---
+++ src/ml_module/data_preprocess.py
@@ -0,0 +1,18 @@
+import cv2
+
+def preprocess_img(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3,3), 0)
+    img = cv2.resize(img, (200,66))
+    img = img/255
+    return img
+
+
+
+
+def data_preprocess(x_train, y_train):
+    for data in x_train:
+        data[0] = preprocess_img(data[0])
+        data[1] = preprocess_img(data[1])
+        data[2] = preprocess_img(data[2])
