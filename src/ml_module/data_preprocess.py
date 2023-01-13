bd4ac528c198b48acd5c2671c6267960642f71a0
Jeremy Neo <doremineo@gmail.com>
2023-01-13 00:47:22 +0000

    changed to 30%

--- src/ml_module/data_preprocess.py
+++ src/ml_module/data_preprocess.py
@@ -1,5 +1,5 @@
 import cv2
-from keras.utils.np_utils import to_categorical
+import numpy as np
 
 def preprocess_img(img):
     img = img[60:135,:,:]
@@ -9,9 +9,19 @@ def preprocess_img(img):
     img = img/255
     return img
 
+def split_data(x_train, y_train):
+    x_train, x_valid = np.split(x_train, [int(len(x_train)*0.3)])
+    y_train, y_valid = np.split(y_train, [int(len(y_train)*0.3)])
+    return x_train, y_train, x_valid, y_valid
+
 
 def data_preprocess(x_train, y_train):
     for data in x_train:
         data[0] = preprocess_img(data[0])
         data[1] = preprocess_img(data[1])
         data[2] = preprocess_img(data[2])
+
+    x_train = np.asarray(x_train)
+    y_train = np.asarray(y_train)
+    x_train, y_train, x_valid, y_valid = split_data(x_train, y_train)
+    return x_train, y_train, x_valid, y_valid
